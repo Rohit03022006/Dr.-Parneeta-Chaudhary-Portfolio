afda569226f64e5e246a95db904ff3472eeeb795
yadavsayan9 <yadavsayan9@gmail.com>
2026-01-09 14:10:03 +0530

    Update Achievements.jsx

--- app/components/Achievements.jsx
+++ app/components/Achievements.jsx
@@ -142,57 +142,7 @@ const Achievements = () => {
       location: "Progressive Horticulture Conclave (PHC 2023), G.B. Pant University of Agriculture and Technology, Pantnagar (3-5th February 2023)",
       prize: "Award"
     },
-    {
-      id: 5,
-      year: 2021,
-      title: "1st Prize in Poster Presentation",
-      organization: "42nd Annual Meeting of Plant Tissue Culture Association (India)",
-      description: "Awarded 1st prize in poster presentation",
-      location: "International Symposium on Advances in Plant Biotechnology and Genome Editing-2021, ICAR-Indian Institute of Agricultural Biotechnology, Ranchi (8th-10th April 2021)",
-      prize: "1st Prize"
-    },
-    {
-      id: 6,
-      year: 2019,
-      title: "3rd Prize in Poster Presentation",
-      organization: "3rd National Conference of Seabuckthorn Association of India",
-      description: "Awarded 3rd prize in poster presentation",
-      location: "Delhi University Botanical Society & Department of Botany, University of Delhi, Delhi (19th-20th December 2019)",
-      prize: "3rd Prize"
-    },
-    {
-      id: 7,
-      year: 2019,
-      title: "Master of Science in Bioinformatics",
-      organization: "Jamia Millia Islamia",
-      description: "First division with Distinction",
-      prize: "Distinction"
-    },
-    {
-      id: 8,
-      year: 2018,
-      title: "Short Term Research Scholarship",
-      organization: "Guru Gobind Singh Indraprastha University",
-      description: "Awarded twice (2018-2020 and 2020-2021) for full term Research Scholar",
-      location: "Office of the Director (Research & Consultancy)",
-      prize: "Scholarship"
-    },
-    {
-      id: 9,
-      year: 2017,
-      title: "Ph.D. Common Entrance Exam Qualified",
-      organization: "Guru Gobind Singh Indraprastha University",
-      description: "Qualified Guru Gobind Indraprastha University Ph.D. common entrance Exam (CET-2017)",
-      prize: "Qualified"
-    },
-    {
-      id: 10,
-      year: 2015,
-      title: "M.Sc. Bioinformatics Entrance Exam Qualified",
-      organization: "Jamia Millia Islamia",
-      description: "Qualified Jamia Millia Islamia Entrance Examination for M.Sc. Bioinformatics",
-      prize: "Qualified"
-    }
+  
   ].sort((a, b) => b.year - a.year)
 
   return (
